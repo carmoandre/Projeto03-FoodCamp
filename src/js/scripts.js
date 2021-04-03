1b64158ba5f610754bb3946bd0c04f8484fd6c32
André Carmo <andre_de11@msn.com>
2021-04-03 12:54:24 -0300

    implementando funcionamento do botão após seleção correta.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,16 +1,27 @@
+let selectedCounter = 0;
+//let selectedsByRow = ["Não há selecionado", "Não há selecionado", "Não há selecionado"];
+
+
 function colorBorder(options, position) {
     if (options.classList.contains("greenBorder") && options.classList.contains(position)) {
         options.classList.remove("greenBorder");
+        selectedCounter--;
+        verifyButtonChange(position);
         return;
     }
 
     if (options.classList.contains("greenBorder") ) {
         options.classList.remove("greenBorder");
+        selectedCounter--;
     }
 
     if (options.classList.contains(position)) {
         options.classList.add("greenBorder");
+        selectedCounter++;
+        verifyButtonChange(position);
     }
+
+    
 }
 
 function showCheckmark(options, position) {
@@ -37,4 +48,28 @@ function selectUnicOption(position, rowClass) {
     const itemRow = document.querySelectorAll(rowClass);
     const iconRow = document.querySelectorAll(rowClass + " ion-icon");
     selectionManager(itemRow, iconRow, position);
+}
+
+function verifyButtonChange(position){
+    const orderButton = document.querySelector(".bottomBarButton");
+    if (selectedCounter === 3) {
+        orderButton.disabled = false;
+        orderButton.classList.add("enabledButton");
+        orderButton.innerHTML = "Fechar pedido";
+        
+    } else {  
+        if (orderButton.classList.contains("enabledButton")) {
+            orderButton.disabled = true;
+            orderButton.classList.remove("enabledButton");
+            orderButton.innerHTML = "Selecione os 3 itens para fechar o pedido";
+        }
+    }
+}
+
+function callConfirmationScreen() {
+    document.querySelector(".confimationScreen").classList.remove("hiddingClass");
+}
+
+function cancelModalAction() {
+    document.querySelector(".confimationScreen").classList.add("hiddingClass");
 }
